1cd6d383deee2894bb94bc41eb510aaa28792e95
gregorywyz <gregwyszynski@gmail.com>
2015-05-06 16:48:06 -0700

    setting up new baby form, waiting for bootsrap_form

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -1,15 +1,24 @@
 class BabiesController < ApplicationController
 
   def index
-  end
-
-  def show
+    @babies = Baby.all
   end
 
   def new
+    @baby = Baby.new
+  end
+
+  def show
+    @baby = Baby.find(params[:id])
   end
 
   def create
+  end
+
+  private
+
+  def baby_params
+    params.require(:creature).permit(:name,:birthday)
   end
 
 end
