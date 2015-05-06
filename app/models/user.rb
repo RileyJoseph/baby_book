25e6554d48c9f00c69e3ff41c70fdb01c041f06c
Ben <wbpiggot@gmail.com>
2015-05-06 16:58:26 -0700

    Functional login, signup created

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ActiveRecord::Base
     presence: true,
     :on => :create
 
+  def self.authenticate email, password
+    User.find_by_email(email).try(:authenticate, password)
+  end
+
 end
