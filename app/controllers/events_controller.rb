5f77dc8cb7e03df8d783043472f4a4260ce5a647
gregorywyz <gregwyszynski@gmail.com>
2015-05-06 18:07:14 -0700

    pushing for Riley to merge

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,20 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.create(event_params)
+    redirect_to @event
   end
 
   private
