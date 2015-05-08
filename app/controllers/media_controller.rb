d1482537a887104b177d9ffe1d9c90afcb232dc9
gregorywyz <gregwyszynski@gmail.com>
2015-05-07 18:19:01 -0700

    problems with CREATE for new media

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -7,7 +7,23 @@ class MediaController < ApplicationController
   end
 
   def create
+    # uploaded_file = params[:baby][:event].path
+    # cloudnary_file = Cloudinary::Uploader.upload(uploaded_file)
 
+    # render json: cloudnary_file
+
+        # create cloudinary url with specific event_id through carrierwave
+    # @event = Event.find(params[:event_id])
+    # @event.media.create(medium_params)
+    # redirect_to baby_events_path(@baby)
+
+    cloudnary_file = Cloudinary::Uploader.upload(medium_params)
+    render json: cloudnary_file
+
+  end
+
+  def medium_params
+    params.require(:medium).permit(:url.to_s)
   end
 
 end
