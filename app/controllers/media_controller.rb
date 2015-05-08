1ba0d684676efb74c26ab1f6c01efc536d196b0a
svillagracia <svillagracia@outlook.com>
2015-05-07 23:34:23 -0700

    Add UI Improvements, link to new_baby_event_medium_path, media create redirects to baby/:id/event/:id

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -12,13 +12,13 @@ class MediaController < ApplicationController
     cloudinary_file = Cloudinary::Uploader.upload(uploaded_file)
 
     # render json: cloudinary_file
-
+    @baby = Baby.find(params[:baby_id])
     @event = Event.find(params[:event_id])
     # render json: @event
     @event.media.create url: cloudinary_file['public_id']
 
     # Medium.create url: cloudinary_file['public_id']
-    redirect_to baby_events_path
+    redirect_to baby_event_path(@baby,@event)
 
   end
 
