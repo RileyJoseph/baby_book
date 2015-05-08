f0bbac1131448d7987fe03a518dfe49b9327abfd
gregorywyz <gregwyszynski@gmail.com>
2015-05-08 14:31:20 -0700

    facebook oauth and current user logic working

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,7 @@ class AuthController < ApplicationController
  def callback
     provider_user = request.env['omniauth.auth']
     # render json: provider_user
+    # render json: params
     #find or create user
     @user = User.find_or_create_by(provider_id:provider_user['uid'],provider:params[:provider]) do |u|
       u.provider_hash = provider_user['credentials']['token']
@@ -12,14 +13,14 @@ class AuthController < ApplicationController
       u.save
       # ^ found in json version of user info
     end
-    # render json: @user
+    render json: @user
     #start session
     session[:user_id] = @user.id
     session[:provider_id] = @user.provider_id
     session[:password_digest] = @user.password_digest
     #send them home
     # render json: session
-    redirect_to babies_path
+    # redirect_to babies_path
   end
 
   def logout
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,15 @@ class User < ActiveRecord::Base
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
+  # validates :password,
+  #   presence: true,
+  #   :on => :create,
+  #   :if => lambda{ new_record? || !password.nil? }
+
   validates :password,
     presence: true,
-    :on => :create
+    :on => :create,
+    :if => lambda{ password_digest != "fb_login" }
 
   def self.authenticate email, password
     User.find_by_email(email).try(:authenticate, password)
