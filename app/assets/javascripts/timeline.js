d8da4fba0fd46e0082e4661f39a59750f427e3b4
Ben <wbpiggot@gmail.com>
2015-05-08 20:19:04 -0700

    Pushing backend data to timeline

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -2,25 +2,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   var container = document.getElementById('visualization');
 
-  // Create a DataSet (allows two way data-binding)
-  var items = new vis.DataSet([
-    {id: 1, content: "<p class='caption'>Asa was born</p>", start: 'October 2, 2013'},
-    {id: 2, content: "<p class='caption'>Asa smiled for the first time</p>", start: 'November 20, 2013'},
-    {id: 3, content: "<p class='caption'>He sat up", start: 'March 12, 2014'},
-    {id: 4, content: "<p class='caption'>Standing up!</p>", start: 'June 19 2014'},
-    {id: 5, content: "<p class='caption'>Asa's first steps</p>", start: 'August 10, 2014'},
-    {id: 6, content: "<p class='caption'>First day at daycare</p>", start: 'September 13, 2014'}
+  var trial = $('.temp_information').data('temp')
 
-  ]);
+  var baby = []
+  for (var i = 0; i < trial.length; i++) {
+    baby[i] = {}
+    baby[i].id = i
+    baby[i].content =  "<p class='caption'>" + trial[i].body + "</p>"
+    baby[i].start = trial[i].date
+  }
+
+  // var baby = [
+  //   {id: 1, content: "<p class='caption'>Asa was born</p>", start: 'October 2, 2013'},
+  //   {id: 2, content: "<p class='caption'>Asa smiled for the first time</p>", start: 'November 20, 2013'},
+  //   {id: 3, content: "<p class='caption'>He sat up", start: 'March 12, 2014'},
+  //   {id: 4, content: "<p class='caption'>Standing up!</p>", start: 'June 19 2014'},
+  //   {id: 5, content: "<p class='caption'>Asa's first steps</p>", start: 'August 10, 2014'},
+  //   {id: 6, content: "<p class='caption'>First day at daycare</p>", start: 'September 13, 2014'}
+
+  // ]
+  // Create a DataSet (allows two way data-binding)
+  var items = new vis.DataSet(baby);
 
   // Configuration for the Timeline
   var options = {
     orientation: "bottom",
-    height: 370,
+    height: 350,
     min: "2013",
     max: "2018",
     start: "2013",
     end: "2015",
+    // moveable: false,
     showCurrentTime: true,
     editable: {
       add: true,         // add new items by double tapping
@@ -40,9 +52,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   $(function(){
     $('.item.box').on('click', function(){
       $("#hidden").slideDown('slow').css("opacity","1")
-    })
+      $("body").prepend("<div id='PopupMask' style='position:fixed;width:100%;height:100%;z-index:10;background-color:gray;'></div>");
+      $("#PopupMask").css('opacity', 0.8);
+      })
     $("#off").on('click', function() {
       $("#hidden").slideUp('slow');
+      setTimeout(function() {$('#PopupMask').fadeOut()}, 300);
     });
   });
 
