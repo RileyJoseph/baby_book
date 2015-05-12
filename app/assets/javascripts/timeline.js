6a11d84aabd3287ebcaea296ee9f46e628be34fe
Ben <wbpiggot@gmail.com>
2015-05-11 19:17:28 -0700

    Adding timeline customization

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,23 +1,27 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
-
-  $('#adjust').on('click', function() {
-    options.moveable = false
-  })
-
-  var m = moment();
-  console.log(m)
 
   var container = document.getElementById('visualization');
 
   var babyInfo = $('.temp_information').data('temp')
 
+
+  for(var i = 0; i < babyInfo.length; i++ ) {
+    var temp = moment()._d
+        if (moment( babyInfo[0].date)._d < temp) {
+          temp = moment( babyInfo[0].date)._d
+        }
+      }
+    var start = moment(temp).subtract(90,"days")._d
+    var present = moment()._d
+    var end = moment(temp).add(5,"years")._d
+
+
   var baby = []
   for (var i = 0; i < babyInfo.length; i++) {
     baby[i] = {}
-    baby[i].id = i
     var showId = babyInfo[i].id
-    baby[i].content =  "<p class='caption' data-toggle='modal' data-target='#myModal' href= events/" + showId + ">" +
+    baby[i].id = i
+    baby[i].content = "<p class='caption' data-toggle='modal' data-target='#myModal' href= events/" + showId + ">" +
                         babyInfo[i].topic + "</p>"
     baby[i].start = babyInfo[i].date
     baby[i].body = babyInfo[i].body
@@ -31,17 +35,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var options = {
     orientation: "bottom",
     height: 250,
-    min: "2013",
-    max: "2018",
-    start: "2013",
-    end: "2015",
-   // moveable: false,
+    min: start,
+    max: end,
+    start: start,
+    end: present,
+    moveable: true,
     showCurrentTime: true,
     editable: {
       add: true,         // add new items by double tapping
       updateTime: true,  // drag items horizontally
-      updateGroup: true // drag items from one group to another
-   // delete an item by tapping the delete button top right
+      updateGroup: true
+
     }
   };
 
@@ -52,11 +56,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
     options
     );
 
+// $('#adjust').on('click', function() {
+
+//   var options = {
+//     orientation: "bottom",
+//     height: 250,
+//     min: start,
+//     max: end,
+//     start: start,
+//     end: present,
+//    moveable: true,
+//     showCurrentTime: true,
+//     editable: {
+//       add: true,         // add new items by double tapping
+//       updateTime: true,  // drag items horizontally
+//       updateGroup: true
+//       moveable: true,// drag items from one group to another
+//    // delete an item by tapping the delete button top right
+//     }
+//   };
+// })
 
 
-  $('#myModal').on('hidden.bs.modal', function(){
-    $(this).removeData('bs.modal');
-  });
+
+
+  // $('#myModal').on('hidden.bs.modal', function(){
+  //   $(this).removeData('bs.modal');
+  // });
 
 
 });
