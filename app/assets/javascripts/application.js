4631dfa289ca71f8685c8bcf3998adf4803632ec
gregorywyz <gregwyszynski@gmail.com>
2015-05-11 18:06:14 -0700

    took jquery out of application.js, commented out cloudinary js config, added root path to brand

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+// require jquery
+//= cloudinary
+//
 //= require jquery_ujs
 //= require_tree .
-//
-//= cloudinary
+
