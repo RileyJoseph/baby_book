edbb8c4092174b6c73eb52aefe927b49d247798e
gregorywyz <gregwyszynski@gmail.com>
2015-05-12 19:45:54 -0700

    added pic to show page

--- app/assets/javascripts/uploader.js
+++ app/assets/javascripts/uploader.js
@@ -1,28 +1,28 @@
 $(function() {
 
-// globally set cloudName
-cloudinary.setCloudName('hfi2i1sfv');
+  // globally set cloudName
+  cloudinary.setCloudName('hfi2i1sfv');
 
-var options = {
-                upload_preset: 'testing',
-                multiple: true,
-                max_files: 10,
-                // folder: 'TestFolder',
-                max_image_width: 900,
-                max_image_height: 900,
-                // form: '#new_medium',
-                // field_name: 'url[]',
-                thumbnails: '.preview',
-                button_class: 'btn btn-success btn-lg',
-                button_caption: '+ Pictures',
-                // keep_widget_open: true,
-                theme: 'minimal'
-              };
+  var uploadOptions = {
+                  upload_preset: 'testing',
+                  multiple: true,
+                  max_files: 10,
+                  // folder: 'TestFolder',
+                  max_image_width: 900,
+                  max_image_height: 900,
+                  // form: '#new_medium',
+                  // field_name: 'url[]',
+                  thumbnails: '.preview',
+                  button_class: 'btn btn-success btn-lg',
+                  button_caption: '+ Pictures',
+                  // keep_widget_open: true,
+                  theme: 'minimal'
+                };
 
   // Open coudinary widget
   // upload to through browser to cloudinary
   // data for media model then added to url[] input field
-  cloudinary.applyUploadWidget(document.getElementById('upload_widget_opener'), options, function(error, result) {
+  cloudinary.applyUploadWidget(document.getElementById('upload_widget_opener'), uploadOptions, function(error, result) {
     console.log(error, result)
     var images = result.map(function(image){
       return image.public_id
