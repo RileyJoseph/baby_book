fd79e82eb0947de527c294d6c18163144f716b4b
svillagracia <svillagracia@outlook.com>
2015-05-13 10:35:58 -0700

    enable modals and crud on new timeline

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -1,30 +1,44 @@
+// Timeline source. Passed through in DOM within createStoryJS in "source".
 var eventObject = {
     "timeline":
     {
         "headline":gon.baby.name+"'s binkline",
         "type":"default",
-        "text":"I was born on "+gon.birthday,
+        "text":"Born on "+gon.birthday,
         // Date format is YYY,DD,MM (strftime("%Y,%m,%e"))
         "startDate":gon.bornDate,
+            "asset": {
+            // This will be the baby's profile picture
+            "media":"http://i.stack.imgur.com/xdkXE.jpg",
+            "credit":"",
+            "caption":"Isn't it adorable?"
+            },
             "date":[]
     }
 };
 
+// Loop that creates ticks in the timeline for ever data object passed through in the returning Events array.
+// Refer to events_controller.rb to see "gon" (gem) variables.
 for (var key in gon.events) {
     var obj = gon.events[key];
     eventObject.timeline.date.push({
                         "startDate":moment.utc(obj.date).format("YYYY,MM,DD").toString(),
                         "headline":obj.topic,
-                        "text":obj.body + "<br><a href='#'>See more Photos</a><br><a href='/babies/" + obj.baby_id + "/events/" + obj.id + "/edit'>Edit</a>",
+                        "text":obj.body + "<br><a data-toggle='modal' data-target='#eventModal' href='/babies/" + obj.baby_id + "/events/" + obj.id + "'>See more</a><br><a data-toggle='modal' data-target='#eventModal' href='/babies/" + obj.baby_id + "/events/" + obj.id + "/edit'>Edit</a>",
+                        "classname":"this-is-a-class-name",
                         "asset":
                         {
+                            // This will be the cover photo for the event, however we determine that.
                             "media":"http://i.stack.imgur.com/xdkXE.jpg",
+                            // This will be a cropped version of the cover photo.
+                            "thumbnail":"http://i.stack.imgur.com/xdkXE.jpg",
                             "credit":"",
                             "caption":""
                         }
                     });
 }
 
+// DOM Loads timeline. JSON is loaded before timeline is rendered.
 $(document).ready(function() {
     createStoryJS({
         type:       'timeline',
