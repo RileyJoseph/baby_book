922125b764ccb2f2af79808312286ca7765c56ce
Ben <wbpiggot@gmail.com>
2015-05-13 19:35:18 -0700

    UI edits

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,8 +1,8 @@
 // $(function() {
-//   $('.imgWrapper').on('click', function(e) {
+//   $('.imgWrapper > p > a').on('click', function(e) {
 //     e.preventDefault();
 //     var delBtn = $(this);
-//     var myUrl = '/maps/' + delBtn.attr('data');
+//     var myUrl = delBtn.attr('href');
 //     console.log(myUrl)
 //     if(confirm('Are you sure you want do delete this?')){
 //     $.ajax({
