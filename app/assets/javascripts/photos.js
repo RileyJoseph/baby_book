529f78c8ff0ac69b7f2e26302b67d024e51e2572
svillagracia <svillagracia@outlook.com>
2015-05-15 11:43:29 -0700

    timeline start on last slide

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -18,11 +18,9 @@
 window.addEventListener("load", function() {
   if (gon.baby) {
     if (gon.baby.gender === "boy") {
-      console.log("its a boy");
       $('.slider-btns').css('background-color','black');
       $('.slider-play-toggle').css('background-color','black');
     } else {
-      console.log("its a girl");
       $('.slider-btns').css('background-color','black');
       $('.slider-play-toggle').addClass('.girl-btns');
     }
